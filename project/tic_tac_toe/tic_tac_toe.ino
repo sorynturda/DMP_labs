aa230f2801d66ea7094a66745380467b26a1a478
Sorin Turda <s.turda12@gmail.com>
2024-11-28 11:37:42 +0200

    logica e buna

--- project/tic_tac_toe/tic_tac_toe.ino
+++ project/tic_tac_toe/tic_tac_toe.ino
@@ -1,7 +1,7 @@
 const int N = 9;
 int flags[N], bs[N], pini_butoane[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8 };
 
-int cnt = 0, x = 1, joc_terminat = 0, cine_castiga;  //castigator x = 1 / 0 = 0
+int cnt = 0, x = 1, joc_terminat = 0, cine_castiga = -1;  //castigator x = 1 / 0 = 0 / -1 = egal
 char mat[3][3] = { { "___" }, { "___" }, { "___" } };
 
 void setup() {
@@ -10,7 +10,7 @@ void setup() {
 }
 
 void loop() {
-  if (joc_terminat == 0) {
+  if (joc_terminat == 0){
     for (int i = 0; i < N; i++) {
       bs[i] = digitalRead(pini_butoane[i]);
       if (bs[i] == LOW && flags[i] == 0) {
@@ -29,7 +29,8 @@ void loop() {
         if (cnt == N) joc_terminat = 1;
       }
     }
-  } else {
+  }
+  else {
     if (cine_castiga == 1)
       Serial.println("X castiga");
     else if (cine_castiga == 0)
@@ -56,33 +57,33 @@ int get_y(int n) {
 void verificare_matrice() {
   int casigator_temp = -1;
   for (int i = 0; i < 3; i++) {
-    if (mat[i][1] == mat[i][0] && mat[i][2] == mat[i][0]) {
+    if (mat[i][1] == mat[i][0] && mat[i][2] == mat[i][0] && mat[0][2] != '_') {
       if (mat[i][0] == 'X')
         casigator_temp = 1;
       else if (mat[i][0] == '0')
         casigator_temp = 0;
     }
-    if (mat[1][i] == mat[0][i] && mat[2][i] == mat[0][i]) {
+    if (mat[1][i] == mat[0][i] && mat[2][i] == mat[0][i] && mat[0][2] != '_') {
       if (mat[0][i] == 'X')
         casigator_temp = 1;
-      else if (mat[i][0] == '0')
+      else if (mat[0][i] == '0')
         casigator_temp = 0;
     }
   }
-  if (mat[1][1] == mat[0][0] && mat[1][1] == mat[2][2]) {  //diagonala principala
+  if (mat[1][1] == mat[0][0] && mat[1][1] == mat[2][2] && mat[0][2] != '_') {  //diagonala principala
     if (mat[1][1] == 'X')
       casigator_temp = 1;
     else if (mat[0][0] == '0')
       casigator_temp = 0;
   }
 
-  if (mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0]) {
+  if (mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0] && mat[0][2] != '_') { // diagonala secundara
     if (mat[1][1] == 'X')
       casigator_temp = 1;
     else if (mat[0][0] == '0')
       casigator_temp = 0;
   }
-  if (cine_castiga != -1) {
+  if (casigator_temp != -1) {
     cine_castiga = casigator_temp;
     joc_terminat = 1;
   }
